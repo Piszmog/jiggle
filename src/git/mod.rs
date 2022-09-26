1126292af3295313ed3bbafc2d5454b3ef12b229
Randell Callahan <piszmogcode@gmail.com>
2022-09-26 14:11:31 -0600

    Update git commands

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,17 +1,147 @@
-use std::process::{Child, Command};
+use std::process::{Command, Stdio};
 
-pub fn checkout(branch: String) -> Child {
-    Command::new("git").arg("checkout").arg(branch).spawn().expect("failed to checkout branch")
+/// TODO
+pub fn checkout(repo_dir: &str, branch: &str) {
+    Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("checkout")
+        .arg(branch)
+        .spawn()
+        .expect("failed to checkout branch")
+        .wait()
+        .unwrap();
 }
 
-pub fn merge(root_branch: String) -> Child {
-    Command::new("git").arg("merge").arg(root_branch).arg("--no-edit").spawn().expect("failed to merge root")
+/// TODO
+pub fn has_conflicts(repo_dir: &str, from_branch: &str) -> bool {
+    let output = Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("merge")
+        .arg(from_branch)
+        .arg("--no-ff")
+        .arg("--no-commit")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to check if there are conflicts")
+        .wait_with_output()
+        .unwrap();
+
+    let message = String::from_utf8(output.stdout).unwrap();
+    let messages = message
+        .split('\n')
+        .map(|s| s.trim());
+
+    // Abort the merge to get to a clean state.
+    abort_merge(repo_dir);
+
+    for message in messages {
+        if message == "Automatic merge failed; fix conflicts and then commit the result." {
+            return true;
+        }
+    }
+    false
 }
 
-pub fn pull() -> Child {
-    Command::new("git").arg("pull").spawn().expect("failed to pull latest")
+fn abort_merge(repo_dir: &str) {
+    Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("merge")
+        .arg("--abort")
+        .spawn()
+        .expect("failed to abort merge")
+        .wait()
+        .unwrap();
 }
 
-pub fn push() -> Child {
-    Command::new("git").arg("push").spawn().expect("failed to push updated code")
+/// TODO
+pub fn is_up_to_date(repo_dir: &str, from_branch: &str) -> bool {
+    let output = Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("branch")
+        .arg("--contains")
+        .arg(from_branch)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to check if branch is up-to-date")
+        .wait_with_output()
+        .unwrap();
+
+    let message = String::from_utf8(output.stdout).unwrap();
+    let branches = message
+        .split('\n')
+        .map(|s| s.trim());
+
+    // Determine which branch we are currently sitting on.
+    let current_branch = get_current_branch(repo_dir);
+
+    // If the current branch has the latest from_branch, we will see the current branch with a '*' prefix.
+    // e.g. "* my-little-branch"
+    let up_to_date_value = format!("* {}", current_branch);
+
+    let mut up_to_date = false;
+    for branch in branches {
+        if branch == current_branch {
+            break;
+        } else if branch == up_to_date_value {
+            up_to_date = true;
+            break;
+        }
+    }
+    up_to_date
+}
+
+fn get_current_branch(repo_dir: &str) -> String {
+    let output = Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("branch")
+        .arg("--show-current")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to get current branch")
+        .wait_with_output()
+        .unwrap();
+    String::from_utf8(output.stdout).unwrap().strip_suffix('\n').unwrap().to_string()
+}
+
+/// TODO
+pub fn merge(repo_dir: &str, from_branch: &str) {
+    Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("merge")
+        .arg(from_branch)
+        .arg("--no-edit")
+        .spawn()
+        .expect("failed to merge root")
+        .wait()
+        .unwrap();
+}
+
+/// TODO
+pub fn pull(repo_dir: &str) {
+    Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("pull")
+        .spawn()
+        .expect("failed to pull latest")
+        .wait()
+        .unwrap();
+}
+
+/// TODO
+pub fn push(repo_dir: &str) {
+    Command::new("git")
+        .arg("-C")
+        .arg(repo_dir)
+        .arg("push")
+        .spawn()
+        .expect("failed to push updated code")
+        .wait()
+        .unwrap();
 }
